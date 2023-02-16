d113e7f3910c3fd5258f6ad986837a9b4b7dc25d
Daniel Alberski <danielalberski@gmail.com>
2023-02-16 19:10:00 +0100

    first commit of remote windows controller app

---
+++ RemoteWindowsController/RemoteWindowsController/ConfigStorage.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using System.ComponentModel.Design.Serialization;
+using System.Configuration.Internal;
+using System.Windows;
+
+namespace RemoteWindowsController
+{
+    public struct ProfileItem
+    {
+        public string name;
+        public string value;
+
+        public ProfileItem(string name, string value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+    }
+
+    public struct Profile
+    {
+        public string name;
+        public List<ProfileItem> items;
+
+        public Profile(string name, List<ProfileItem> items)
+        {
+            this.name = name;
+            this.items = items;
+        }
+    }
+
+    public struct AppShortcut
+    {
+        public string name;
+        public string path;
+        public string args;
+
+        public AppShortcut(string name, string path,string args)
+        {
+            this.name = name;
+            this.path = path;
+            this.args = args;
+        }
+    }
+
+    public class ConfigStorage
+    {
+        private const string FILE_NAME = "config-rwc.xml";
+        private const int MAX_PROFILES = 6;
+        private const int MAX_APPS = 6;
+
+        private List<Profile> profiles;
+        private List<AppShortcut> appShortcuts;
+
+        public ConfigStorage()
+        {
+            profiles = new List<Profile>() { generateInitialProfile() };
+            appShortcuts = new List<AppShortcut> { generateInitialAppShortcut() };
+            if (System.IO.File.Exists(FILE_NAME))
+            {
+                readConfigFile();
+            } else {
+                writeConfigFile();
+            }
+        }
+
+        private Profile generateInitialProfile()
+        {
+            List<ProfileItem> items = new List<ProfileItem>();
+            for (int i = 1; i < 7; i++) items.Add(new ProfileItem("Slot " + i, ""));
+            return new Profile("New profile", items);
+        }
+
+        private AppShortcut generateInitialAppShortcut()
+        {
+            return new AppShortcut("New app", "calc.exe", "");
+        }
+
+        private void readConfigFile()
+        {
+            profiles = new List<Profile>();
+            appShortcuts = new List<AppShortcut>();
+
+            XmlDocument xml = new XmlDocument();
+            xml.Load(FILE_NAME);
+
+            XmlNodeList profileNodes = xml.GetElementsByTagName("Profile");
+            foreach (XmlNode profileNode in profileNodes)
+            {
+                List<ProfileItem> items = new List<ProfileItem>();
+                foreach(XmlNode itemNode in profileNode.ChildNodes)
+                {
+                    if (itemNode is XmlElement el) items.Add(new ProfileItem(el.GetAttribute("name"), el.GetAttribute("value")));
+                }
+                if (profileNode is XmlElement profileElement) profiles.Add(new Profile(profileElement.GetAttribute("name"), items));
+            }
+
+            XmlNodeList appNodes = xml.GetElementsByTagName("App");
+            foreach (XmlNode appNode in appNodes)
+            {
+                if (appNode is XmlElement appElement)
+                {
+                    string name = appElement.GetAttribute("name");
+                    string path = appElement.GetAttribute("path");
+                    string args = appElement.GetAttribute("args");
+                    appShortcuts.Add(new AppShortcut(name, path, args));
+                }
+            }
+        }
+
+        private void writeConfigFile()
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.AppendChild(xml.CreateComment("Config file of the Remote Windows Controller app"));
+            XmlNode root = xml.CreateElement("Config");
+
+            XmlNode profileRoot = xml.CreateElement("Profiles");
+            foreach (Profile profile in profiles)
+            {
+                XmlNode profileNode = xml.CreateElement("Profile");
+                XmlAttribute nameAttribute = xml.CreateAttribute("name");
+                nameAttribute.Value = profile.name;
+                profileNode.Attributes?.Append(nameAttribute);
+                foreach(ProfileItem item in profile.items)
+                {
+                    XmlNode itemNode = xml.CreateElement("ProfileItem");
+                    XmlAttribute itemNameAttribute = xml.CreateAttribute("name");
+                    XmlAttribute itemValueAttribute = xml.CreateAttribute("value");
+                    itemNameAttribute.Value = item.name;
+                    itemValueAttribute.Value = item.value;
+                    itemNode.Attributes?.Append(itemNameAttribute);
+                    itemNode.Attributes?.Append(itemValueAttribute);
+                    profileNode.AppendChild(itemNode);
+                }
+                profileRoot.AppendChild(profileNode);
+            }
+            root.AppendChild(profileRoot);
+
+            XmlNode appShortcutsRoot = xml.CreateElement("Apps");
+            foreach (AppShortcut appShortcut in appShortcuts)
+            {
+                XmlNode appShortcutNode = xml.CreateElement("App");
+                XmlAttribute nameAttribute = xml.CreateAttribute("name");
+                XmlAttribute pathAttribute = xml.CreateAttribute("path");
+                XmlAttribute argsAttribute = xml.CreateAttribute("args");
+                nameAttribute.Value = appShortcut.name;
+                pathAttribute.Value = appShortcut.path;
+                argsAttribute.Value = appShortcut.args;
+                appShortcutNode.Attributes?.Append(nameAttribute);
+                appShortcutNode.Attributes?.Append(pathAttribute);
+                appShortcutNode.Attributes?.Append(argsAttribute);
+                appShortcutsRoot.AppendChild(appShortcutNode);
+            }
+            root.AppendChild(appShortcutsRoot);
+
+            xml.AppendChild(root);
+            xml.Save(FILE_NAME);
+        }
+
+        private void showError(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private bool swapElements<T>(IList<T> list, int indexA, int indexB)
+        {
+            if (indexA < 0 || indexA >= list.Count) return false;
+            if (indexB < 0 || indexB >= list.Count) return false;
+            T tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+            return true;
+        }
+
+        public List<Profile> getProfiles() => profiles;
+
+        public bool addProfile()
+        {
+            if(profiles.Count >= MAX_PROFILES)
+            {
+                showError("You can have max " + MAX_PROFILES + " saved profiles!");
+                return false;
+            }
+            profiles.Add(generateInitialProfile());
+            writeConfigFile();
+            return true;
+        }
+
+        public bool removeProfile(int idx)
+        {
+            if(idx < profiles.Count && idx >= 0)
+            {
+                if(profiles.Count == 1)
+                {
+                    showError("You can't remove the last saved profile!");
+                    return false;
+                }
+                profiles.RemoveAt(idx);
+                writeConfigFile();
+                return true;
+            }
+            return false;
+        }
+
+        public bool updateProfile(int idx, Profile profile)
+        {
+            if (idx < profiles.Count && idx >= 0)
+            {
+                profiles[idx] = profile;
+                writeConfigFile();
+                return true;
+            }
+            return false;
+        }
+
+        public bool moveProfile(int idx, int dir)
+        {
+            if(swapElements<Profile>(profiles, idx, idx + dir))
+            {
+                writeConfigFile();
+                return true;
+            }
+            return false;
+        }
+
+        public List<AppShortcut> getAppShortcuts() => appShortcuts;
+
+        public bool addAppShortcut()
+        {
+            if (appShortcuts.Count >= MAX_APPS)
+            {
+                showError("You can have max " + MAX_APPS + " saved apps!");
+                return false;
+            }
+            appShortcuts.Add(generateInitialAppShortcut());
+            writeConfigFile();
+            return true;
+        }
+
+        public bool removeAppShortcut(int idx)
+        {
+            if (idx < appShortcuts.Count && idx >= 0)
+            {
+                if (appShortcuts.Count == 1)
+                {
+                    showError("You can't remove the last saved app shortcut!");
+                    return false;
+                }
+                appShortcuts.RemoveAt(idx);
+                writeConfigFile();
+                return true;
+            }
+            return false;
+        }
+
+        public bool updateAppShortcut(int idx, AppShortcut appShortcut)
+        {
+            if (idx < appShortcuts.Count && idx >= 0)
+            {
+                appShortcuts[idx] = appShortcut;
+                writeConfigFile();
+                return true;
+            }
+            return false;
+        }
+
+        public bool moveAppShortcut(int idx, int dir)
+        {
+            if (swapElements<AppShortcut>(appShortcuts, idx, idx + dir))
+            {
+                writeConfigFile();
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ RemoteWindowsController/RemoteWindowsController/StorageManager.xaml.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RemoteWindowsController
+{
+    /// <summary>
+    /// Interaction logic for StorageManager.xaml
+    /// </summary>
+    public partial class StorageManager : Window
+    {
+        ConfigStorage config;
+        TextBox[] nameFieldsRef = new TextBox[6];
+        TextBox[] valueFieldsRef = new TextBox[6];
+
+        bool profileModified = false;
+        bool appModified = false;
+
+        public StorageManager()
+        {
+            config = ((App)Application.Current).config;
+            InitializeComponent();
+            refreshListsOfProfilesAndApps();
+        }
+
+        void refreshListsOfProfilesAndApps()
+        {
+            int selectedProfile = profilesList.SelectedIndex;
+            int selectedApp = appsList.SelectedIndex;
+            profilesList.SelectionChanged -= list_SelectionChanged;
+            appsList.SelectionChanged -= list_SelectionChanged;
+            profilesList.Items.Clear();
+            appsList.Items.Clear();
+
+            List<Profile> profiles = config.getProfiles();
+            List<AppShortcut> appShortcuts = config.getAppShortcuts();
+
+            for (int i = 0; i < profiles.Count; i++)
+            {
+                profilesList.Items.Add("(" + (i + 1) + "/" + profiles.Count + ") " + profiles[i].name);
+            }
+            for (int i = 0; i < appShortcuts.Count; i++)
+            {
+                appsList.Items.Add("(" + (i + 1) + "/" + appShortcuts.Count + ") " + appShortcuts[i].name);
+            }
+
+            profilesList.SelectedIndex = selectedProfile < 0 || selectedProfile >= profiles.Count ? 0 : selectedProfile;
+            appsList.SelectedIndex = selectedApp < 0 || selectedApp >= appShortcuts.Count ? 0 : selectedApp;
+            refreshProfileFields();
+            refreshAppFields();
+            profilesList.SelectionChanged += list_SelectionChanged;
+            appsList.SelectionChanged += list_SelectionChanged;
+        }
+
+        private void list_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            refreshListsOfProfilesAndApps();
+        }
+
+        void refreshProfileFields()
+        {
+            Profile currentProfile = config.getProfiles()[profilesList.SelectedIndex];
+            profileName.TextChanged -= profileDataChanged;
+            profileName.Text = currentProfile.name;
+            profileName.TextChanged += profileDataChanged;
+            profileItemsContainer.Children.Clear();
+            for (int i = 0; i < 6; i++)
+            {
+                ProfileItem item = currentProfile.items[i];
+                Label lbl = new Label() {
+                    Margin = new Thickness(5, 10, 5, 5),
+                    Content = "- Item " + (i + 1).ToString() + " -",
+                    HorizontalContentAlignment = HorizontalAlignment.Center,
+                };
+                TextBox nameInput = new TextBox()
+                {
+                    Margin = new Thickness(5),
+                    MaxLength = 20,
+                    Text = item.name,
+                };
+                TextBox valueInput = new TextBox()
+                {
+                    Margin = new Thickness(5),
+                    MaxLength = 150,
+                    Text = item.value,
+                };
+                nameInput.TextChanged += profileDataChanged;
+                valueInput.TextChanged += profileDataChanged;
+                nameFieldsRef[i] = nameInput;
+                valueFieldsRef[i] = valueInput;
+                profileItemsContainer.Children.Add(lbl);
+                profileItemsContainer.Children.Add(nameInput);
+                profileItemsContainer.Children.Add(valueInput);
+            }
+            refreshProfileButtons(false);
+        }
+
+        private void profileDataChanged(object sender, TextChangedEventArgs e)
+        {
+            refreshProfileButtons(true);
+        }
+
+        private void refreshProfileButtons(bool s)
+        {
+            profileModified = s;
+            saveProfile.IsEnabled = s;
+            restoreProfile.IsEnabled = s;
+            newProfile.IsEnabled = !s;
+            deleteProfile.IsEnabled = !s;
+            moveUpProfile.IsEnabled = !s;
+            moveDownProfile.IsEnabled = !s;
+        }
+
+        void refreshAppFields()
+        {
+            AppShortcut currentAppShortcut = config.getAppShortcuts()[appsList.SelectedIndex];
+            appName.TextChanged -= appDataChanged;
+            appPath.TextChanged -= appDataChanged;
+            appArgs.TextChanged -= appDataChanged;
+            appName.Text = currentAppShortcut.name;
+            appPath.Text = currentAppShortcut.path;
+            appArgs.Text = currentAppShortcut.args;
+            appName.TextChanged += appDataChanged;
+            appPath.TextChanged += appDataChanged;
+            appArgs.TextChanged += appDataChanged;
+            refreshAppButtons(false);
+        }
+
+        private void appDataChanged(object sender, TextChangedEventArgs e)
+        {
+            refreshAppButtons(true);
+        }
+
+        private void refreshAppButtons(bool s)
+        {
+            appModified = s;
+            saveApp.IsEnabled = s;
+            restoreApp.IsEnabled = s;
+            newApp.IsEnabled = !s;
+            deleteApp.IsEnabled = !s;
+            moveUpApp.IsEnabled = !s;
+            moveDownApp.IsEnabled = !s;
+        }
+
+        private void saveProfile_Click(object sender, RoutedEventArgs e)
+        {
+            List<ProfileItem> items = new List<ProfileItem>();
+            for(int i = 0; i < nameFieldsRef.Length; i++)
+            {
+                items.Add(new ProfileItem(nameFieldsRef[i].Text.Trim(), valueFieldsRef[i].Text.Trim()));
+            }
+            config.updateProfile(profilesList.SelectedIndex, new Profile(profileName.Text.Trim(), items));
+            refreshListsOfProfilesAndApps();
+        }
+
+        private void restoreProfile_Click(object sender, RoutedEventArgs e)
+        {
+            refreshListsOfProfilesAndApps();
+        }
+
+        private void newProfile_Click(object sender, RoutedEventArgs e)
+        {
+            if(config.addProfile())
+            {
+                refreshListsOfProfilesAndApps();
+                profilesList.SelectedIndex = profilesList.Items.Count - 1;
+            }
+        }
+
+        private void deleteProfile_Click(object sender, RoutedEventArgs e)
+        {
+            if (config.removeProfile(profilesList.SelectedIndex))
+            {
+                refreshListsOfProfilesAndApps();
+            }
+        }
+
+        private void moveUpProfile_Click(object sender, RoutedEventArgs e)
+        {
+            if (config.moveProfile(profilesList.SelectedIndex, -1))
+            {
+                refreshListsOfProfilesAndApps();
+                profilesList.SelectedIndex -= 1;
+            }
+        }
+
+        private void moveDownProfile_Click(object sender, RoutedEventArgs e)
+        {
+            if (config.moveProfile(profilesList.SelectedIndex, 1))
+            {
+                refreshListsOfProfilesAndApps();
+                profilesList.SelectedIndex += 1;
+            }
+        }
+
+        private void saveApp_Click(object sender, RoutedEventArgs e)
+        {
+            config.updateAppShortcut(appsList.SelectedIndex, new AppShortcut(appName.Text.Trim(), appPath.Text.Trim(), appArgs.Text.Trim()));
+            refreshListsOfProfilesAndApps();
+        }
+
+        private void restoreApp_Click(object sender, RoutedEventArgs e)
+        {
+            refreshListsOfProfilesAndApps();
+        }
+
+        private void newApp_Click(object sender, RoutedEventArgs e)
+        {
+            if (config.addAppShortcut())
+            {
+                refreshListsOfProfilesAndApps();
+                appsList.SelectedIndex = appsList.Items.Count - 1;
+            }
+        }
+
+        private void deleteApp_Click(object sender, RoutedEventArgs e)
+        {
+            if (config.removeAppShortcut(appsList.SelectedIndex))
+            {
+                refreshListsOfProfilesAndApps();
+            }
+        }
+
+        private void moveUpApp_Click(object sender, RoutedEventArgs e)
+        {
+            if (config.moveAppShortcut(appsList.SelectedIndex, -1))
+            {
+                refreshListsOfProfilesAndApps();
+                appsList.SelectedIndex -= 1;
+            }
+        }
+
+        private void moveDownApp_Click(object sender, RoutedEventArgs e)
+        {
+            if (config.moveAppShortcut(appsList.SelectedIndex, 1))
+            {
+                refreshListsOfProfilesAndApps();
+                appsList.SelectedIndex += 1;
+            }
+        }
+    }
+}
